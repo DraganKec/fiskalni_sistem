64233d4b9173b4e3cbf8e9b9b0d90bf9908dd0e1
DraganKec <Omen1983>
2017-02-03 22:28:28 +0100

    Add Gulp to project

--- src/main/resources/static/komitent/KomitentController.js
+++ src/main/resources/static/komitent/KomitentController.js
@@ -1,3 +1,19 @@
-/**
- * Created by Dragan on 19/12/16.
- */
+angular.module('fiskalniSistem')
+    .controller('addKomitentCtrl', ['$scope', '$http', 'komitentService',
+        function ($scope, $http, komitentService) {
+
+            $scope.saveKomitent = function (komitent) {
+                komitentService.saveKomitent(komitent);
+            };
+
+        }])
+    .controller('allKomitentsCtrl', ['$scope', '$http', 'komitentService',
+        function ($scope, $http, komitentService) {
+
+            komitentService.getAllKomitent().then(function (data) {
+                $scope.allKomitent = data;
+            });
+            $scope.deleteKomitent = function (id) {
+                komitentService.deleteKomitent(id);
+            };
+        }]);
---
+++ src/main/resources/static/komitent/komitentService.js
@@ -0,0 +1,52 @@
+/**
+    * Created by Dragan on 03/02/17.
+    */
+angular.module('fiskalniSistem')
+    .factory('komitentService', function ($http) {
+
+        return {
+            getAllKomitent: function () {
+                return $http({
+                    method: "GET",
+                    url: "/getAllKomitents"
+                }).then(function (response) {
+                    return response.data;
+                });
+            },
+            getNazivKomitenta: function () {
+                return $http({
+                    method: "GET",
+                    url: "/getNazivKomitenta"
+                }).then(function (response) {
+                    return response.data;
+                });
+            },
+            saveKomitent: function (komitent) {
+                return $http({
+                    method: 'POST',
+                    url: '/saveKomitent',
+                    data: komitent
+                }).then(function (result) {
+                    return result.data;
+                })
+            },
+            deleteKomitent: function (id) {
+                return $http({
+                    method: 'POST',
+                    url: '/deleteKomitent',
+                    data: id
+                }).then(function (result) {
+                    return result.data;
+                });
+            },
+            getKomitent: function (id) {
+                return $http({
+                    method: 'POST',
+                    url: '/getOneKomitent',
+                    data: id
+                }).then(function (response) {
+                    return response.data;
+                });
+            }
+        }
+    });
